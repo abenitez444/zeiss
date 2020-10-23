955cb8bd220e1d2fb7e4c9a8a061b35bf218b9c9
Joisamar Garcia <jgarcia@nearsource.net>
2020-10-23 12:45:38 -0400

    commit - Integración pasarela de pagos - Vista faltante

---
+++ resources/views/admin/facturas/payment_invoice.blade.php
@@ -0,0 +1,37 @@
+@extends('admin.layouts.dashboard')
+
+@section('content')
+
+<!-- Begin Page Content -->
+<div class="container-fluid">
+    <h1>
+		Pagar Facturas
+    </h1>
+
+    <div class="row">
+        <div class="col-md-6 col-xs-12">
+            <div class="card">
+                <div class="card-header">
+                    <h4>Total: {{$amount}}</h4>
+                    <h4>Identificador de tu pago: {{$referencia}}</h4>
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">BBVA Miltipagos Express</h5>
+                    <form action="https://www.adquiramexico.com.mx:443/mExpress/pago/avanzado" method="post"/>
+                        <input type="hidden" name="importe" value="{{$amount}}"/>
+                        <input type="hidden" name="referencia" value="{{$referencia}}"/>
+                        <input type="hidden" name="urlretorno" value="http://appservertest.tk/admin/facturas/comprobante-pago"/>
+                        <input type="hidden" name="idexpress" value="1809"/>
+                        <input type="hidden" name="financiamiento" value="0"/>
+                        <input type="hidden" name="plazos" value=""/>
+                        <input type="hidden" name="mediospago" value="110000"/>
+                        <input type="hidden" name="signature" value="{{$signature}}"/>
+                        <input type="image" src="https://dicff9jl33o1o.cloudfront.net/verticales/bexpress/resources/img/icon/paybutton_4.png" />
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@stop
